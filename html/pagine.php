87cd815d27aaaadce4d4e815da74d07360399a0b
thenewsniper <61515795+thenewsniper@users.noreply.github.com>
2020-06-09 19:01:13 +0100

    html

---
+++ html/pagine.php
@@ -0,0 +1,21 @@
+<html>
+    <head></head>
+
+    <body>
+        <?php
+        $file = fopen("esempio.txt","r");
+            if(!$file){
+                echo("errore");
+            }
+            for ($i=0; $i < 4 ; $i++) { 
+
+                $data = fread($file, 5);
+                echo($data);
+            
+            }
+               
+          fclose($file);
+        ?>
+    </body>
+
+</hmtl>
